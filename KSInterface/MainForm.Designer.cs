55ec2176f16055312ed1d7b3f2a1a1c7aeb27ec6
Vvalter <sr@mail25.de>
2014-02-19 00:31:22 +0100

    Implemented Logging

---
+++ KSInterface/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace KSInterface
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.End = new System.Windows.Forms.Button();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(13, 13);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // End
+            // 
+            this.End.Location = new System.Drawing.Point(94, 13);
+            this.End.Name = "End";
+            this.End.Size = new System.Drawing.Size(75, 23);
+            this.End.TabIndex = 1;
+            this.End.Text = "End";
+            this.End.UseVisualStyleBackColor = true;
+            this.End.Click += new System.EventHandler(this.End_Click);
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.rtbLog.Location = new System.Drawing.Point(13, 43);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.ReadOnly = true;
+            this.rtbLog.Size = new System.Drawing.Size(156, 254);
+            this.rtbLog.TabIndex = 2;
+            this.rtbLog.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(182, 309);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.End);
+            this.Controls.Add(this.Start);
+            this.Name = "MainForm";
+            this.Text = "Keystone";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button End;
+        private System.Windows.Forms.RichTextBox rtbLog;
+    }
+}
+
---
+++ KSInterface/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KSInterface
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Log("Initialized\n");
+        }
+
+        private void Log(string text, string colorName = "Black")
+        {
+            rtbLog.SelectionColor = Color.FromName(colorName);
+            rtbLog.AppendText(DateTime.Now.ToString("HH:mm:ss")+ ":  ");
+            rtbLog.AppendText(text);
+            rtbLog.ScrollToCaret();
+            rtbLog.Update();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Log("Started\n");
+        }
+
+        private void End_Click(object sender, EventArgs e)
+        {
+            Log("Terminated\n");
+        }
+    }
+}
