d4ca23549fa9328bb1989ee434c1e2edd4683151
Vvalter <sr@mail25.de>
2014-02-19 19:23:52 +0100

    P/Invoke working

---
+++ KSDll/KSDll.cpp
@@ -0,0 +1,10 @@
+// KSDll.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "KSDll.h"
+
+KSDLL_API int fnKSDll(int a, int b)
+{
+	return 42;
+}
--- KSInterface/MainForm.cs
+++ KSInterface/MainForm.cs
@@ -7,34 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace KSInterface
 {
     public partial class MainForm : Form
     {
+        [DllImport("KSDll.dll", EntryPoint="#1")]
+        public static extern int KSDLLadd(int a, int b);
+
         public MainForm()
         {
             InitializeComponent();
-            Log("Initialized\n");
+            Log("Initialized");
+            Log("1+1: " + KSDLLadd(1, 1));       
         }
 
         private void Log(string text, string colorName = "Black")
         {
             rtbLog.SelectionColor = Color.FromName(colorName);
             rtbLog.AppendText(DateTime.Now.ToString("HH:mm:ss")+ ":  ");
-            rtbLog.AppendText(text);
+            rtbLog.AppendText(text + "\n");
             rtbLog.ScrollToCaret();
             rtbLog.Update();
         }
 
         private void Start_Click(object sender, EventArgs e)
         {
-            Log("Started\n");
+            Log("Started");
         }
 
         private void End_Click(object sender, EventArgs e)
         {
-            Log("Terminated\n");
+            Log("Terminated");
+        }
+
+        private void rtbLog_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
